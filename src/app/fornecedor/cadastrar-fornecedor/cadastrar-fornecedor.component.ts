6fdf0be2665713a2070ac48f3d8b6976da8441f0
Thiago <thiagogambati@outlook.com>
2020-11-02 00:19:19 -0300

    Projeto Fujioka

---
+++ src/app/fornecedor/cadastrar-fornecedor/cadastrar-fornecedor.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastrar-fornecedor',
+  templateUrl: './cadastrar-fornecedor.component.html',
+  styleUrls: ['./cadastrar-fornecedor.component.css']
+})
+export class CadastrarFornecedorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // tslint:disable-next-line:typedef
+  fornecedor(form: NgForm){
+    const infoFornecedor = {
+      nome : [form.value.nomeFunc],
+      cpnj : [form.value.cpnjFornecedor],
+      endereco : [form.value.endereco],
+      Produto : [form.value.produto]};
+
+    console.log(infoFornecedor); }
+  /* public salvarBD(infoFornecedor){
+INSERT INTO fornecedor(nome_BD, cnpj_BD, endereco_BD, produto_BD)
+  }
+*/
+}
---
+++ src/app/fornecedor/lista-fornecedor/lista-fornecedor.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-fornecedor',
+  templateUrl: './lista-fornecedor.component.html',
+  styleUrls: ['./lista-fornecedor.component.css']
+})
+export class ListaFornecedorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // tslint:disable-next-line:typedef
+  listarFuncionario(){
+    /* Função para futuramente puxar dados do BD
+    const dadosbd = SELECT * FROM  funcionario
+
+    return dadosbd;*/}
+
+  // tslint:disable-next-line:typedef
+  getBD(){/*
+
+    const nome = nome_BD
+    const cnpj = cnpj_BD
+    const endereco = endereco_BD
+    const produto = produto_BD
+
+  */}
+}
